93844f7e32b8f41b58da064324661c8a505a08b2
Phinocio <github@phinocio.com>
2024-04-18 13:49:35 -0600

    feat: comparison list 1 work, filter list

--- src/routes/(app)/compare/+page.svelte
+++ src/routes/(app)/compare/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-	import InfoIcon from '$lib/components/icons/Info.svelte';
 	import VerifiedIcon from '$lib/components/icons/Verified.svelte';
+	import type { List } from '$lib/types/List';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	let filterText: string;
+	const { lists: allLists } = data;
 
-	const { lists } = data;
+	// Need to copy this to a new variable to not mutate the original when filtering.
+	// This allows us to use .filter() on content and also have it show files again
+	// when deleting a search, instead of mutating it with content = content.filter()
+	// and ending up forcing the user to refresh the page to see original lines again.
+	// Also the only important thing for displaying the lists, is the array of lists
+	// contained in lists.data.
+	let lists: List[] = allLists.data;
+
+	function filterLists() {
+		const filter = filterText.toLowerCase();
+		lists = allLists.data.filter((list: List) => {
+			return (
+				list.name.toLowerCase().includes(filter) ||
+				list.game.name.toLowerCase().includes(filter) ||
+				list.author?.name.toLowerCase().includes(filter)
+			);
+		});
+	}
 </script>
 
 <svelte:head>
@@ -13,13 +32,19 @@
 </svelte:head>
 
 <h1 class="mb-4 text-3xl font-bold">Compare Lists</h1>
-<h2 class="text-2xl font-bold">Choose the First List</h2>
-<p class="my-4 flex rounded-xl bg-blue-500 p-4 text-white">
-	<InfoIcon class="mr-2 h-6 w-6" /> This list will act as the "source of truth" for comparisons.
-</p>
+<h2 class="mb-4 text-2xl font-bold">Choose the First List</h2>
 
-<article class="grid grid-cols-1 gap-10 md:grid-cols-3">
-	{#each lists.data as list}
+<input
+	type="text"
+	class="mb-8 flex w-full rounded-xl bg-gray-200 p-4 focus:outline-none focus:ring-1 focus:ring-blue-500 dark:bg-[#26263a] md:w-auto"
+	placeholder="Filter..."
+	id="filter-lists"
+	bind:value={filterText}
+	on:input={filterLists}
+/>
+
+<article class="grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-4">
+	{#each lists as list}
 		<section
 			class="flex justify-between rounded-xl bg-gray-200 p-4 text-text-light dark:bg-[#26263a] dark:text-text-dark"
 		>
